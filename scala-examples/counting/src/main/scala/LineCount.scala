552b5f439943b5d65a5c35abf2f2f7911ca4e93a
nikos-daniilidis <nikos.daniilidis@gmail.com>
2015-06-01 22:58:49 -0700

    Added instructions to standalone scala appliication

--- scala-examples/counting/src/main/scala/LineCount.scala
+++ scala-examples/counting/src/main/scala/LineCount.scala
@@ -3,6 +3,25 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 
 object LineCount {
+	// To run this program as an application:
+	// 1. Create a build.sbt file with:
+	//		- name and version of the application
+	//		- scala version
+	//		- spark version
+	// 2. Directory  structure should be:
+	//		application/
+	//			build.sbt
+	//			/src/main/scala/yourApplicationScript.scala
+	// 3. Build the application using sbt. From the application directory:
+	// 		sbt clean package
+	//	  sbt will build a package and print the path on terminal, e.g.:
+	//		~/code/spark-kkwz/scala-examples/counting-test/target/scala-2.10/learning-spark-mini-example_2.10-0.0.1.jar
+	// 4. Run the application using spark-submit
+	//		$SPARK_HOME/bin/spark-submit --class "AppName" ./target/...(as above) ./firstArgument ./secondArgument
+	//	  e.g. 
+	//		~/spark-1.3.1-bin-hadoop2.4/bin/spark-submit --class "LineCount" ./target/scala-2.10/learning-spark-mini-example_2.10-0.0.1.jar ./build.sbt ./linecounts
+	//	  Output is: 
+	//		There are 10 lines in file ./build.sbt     
 	def main(args: Array[String]) {
 		val inputFile = args(0)
 		val outputFile = args(1)
